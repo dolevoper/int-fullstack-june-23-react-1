e32c7d04e50862ee892fc9c6ce6b26a13af599e8
Omer Dolev <omerdolev90@gmail.com>
2024-01-25 20:35:59 +0200

    active page solution

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css';
 import { PropsWithChildren } from "react";
 
 function App() {
-  const userChoice = prompt("Please enter 1 or 2");
+  // const userChoice = prompt("Please enter 1 or 2");
   // const userChoice = "2";
 
   // if (userChoice === "1") {
@@ -39,14 +39,16 @@ function App() {
 
   return (
     <main>
-      <Menu />
+      <Menu activePage="home" />
+      <Menu activePage="about" />
+      <Menu activePage="contact" />
       <h1>Hello world</h1>
       <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse, aliquid?</p>
-      {userChoice === "1" ?
+      {/* {userChoice === "1" ?
         <p>1</p> :
         userChoice === "2" ?
           <p>2</p> :
-          undefined}
+          undefined} */}
       {foo}
       <Button className="primary" size={1.5}>Sign up now</Button>
       <Button className="secondary" size={1.2}>More details</Button>
@@ -74,17 +76,50 @@ function Button({ className, size = 1, children }: PropsWithChildren<ButtonProps
     </button>;
 }
 
-function Menu() {
+type MenuProps = {
+  activePage: "home" | "about" | "contact"
+};
+
+// <Menu activePage="home" />
+
+function Menu({ activePage }: MenuProps) {
+  // if (activePage === "home") {
+  //   return <menu style={{
+  //     display: "flex",
+  //     gap: "1rem",
+  //     padding: "0",
+  //     listStyle: "none"
+  //   }}>
+  //     <li>Home</li>
+  //     <li><a href="">About</a></li>
+  //     <li><a href="">Contact us</a></li>
+  //   </menu>;
+  // }
+
+  // if (activePage === "about") {
+  //   return <menu style={{
+  //     display: "flex",
+  //     gap: "1rem",
+  //     padding: "0",
+  //     listStyle: "none"
+  //   }}>
+  //     <li><a href="">Home</a></li>
+  //     <li>About</li>
+  //     <li><a href="">Contact us</a></li>
+  //   </menu>
+  // }
+
   return <menu style={{
     display: "flex",
     gap: "1rem",
     padding: "0",
-    listStyle: "none"
+    listStyle: "none",
+    justifyContent: "space-between"
   }}>
-    <li><a href="">Home</a></li>
-    <li><a href="">About</a></li>
-    <li><a href="">Contact us</a></li>
-  </menu>
+    <li>{activePage === "home" ? <span>Home</span> : <a href="">Home</a>}</li>
+    <li>{activePage === "about" ? <span>About</span> : <a href="">About</a>}</li>
+    <li>{activePage === "contact" ? <span>Contact us</span> : <a href="">Contact us</a>}</li>
+  </menu>;
 }
 
 export default App;
